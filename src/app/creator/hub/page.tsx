0b506988ea49a5602d36a6a6c2f95df86736a2ef
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-09-17 19:10:58 -0400

    applying own reusable components and adjusting mobile responsible

--- src/app/creator/hub/page.tsx
+++ src/app/creator/hub/page.tsx
@@ -1,31 +1,12 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { Youtube } from "lucide-react";
-import Link from "next/link";
+import HubCard from "@/components/common/HubCard";
 
 export default function CreatorHub() {
     return (
         <>
-            <main className="gap-16 grid grid-cols-6 max-md:grid-cols-2 max-xl:grid-cols-4 my-10 mx-16">
-                <Link href={"/creator/youtube-resource"} passHref legacyBehavior>
-                    <div className="relative group">
-                        <Button className="group-hover:opacity-20 w-full h-full pt-2 shadow-lg shadow-purple-950 bg-purple-950 flex flex-col">
-                            <Youtube size={70} className="mb-6 mt-1 max-sm:h-1/3"/>
-                            Youtube Creation
-                        </Button> 
-                        <div className="group-hover:visible group-hover:opacity-100 absolute top-0 bottom-0 left-0 right-0 opacity-0 flex items-center justify-center flex-col py-5 text-xs font-bold transition-opacity ease-in duration-200 cursor-pointer">
-                            <p>As a youtuber you can:</p>
-                            <br/>
-                            <ul>
-                                <li>&#8226; Create a title</li>
-                                <li>&#8226; Create a description</li>
-                                <li>&#8226; Generate a summary</li>
-                            </ul>
-                            <br/>  
-                            <p>Everything from 1 video</p>
-                        </div>
-                    </div>
-                </Link>
+            <main className="gap-16 grid grid-cols-6 max-md:grid-cols-2 max-xl:grid-cols-4 max-xl:mx-4 my-10 mx-16">
+                {/* to see what icon use link to https://lucide.dev/icons/ */}
+                <HubCard hasHoverContent name="Youtube Creator" icon="youtube" linkTo="/creator/youtube-resource" bodyDescription="Create a title-Create a description-Generate a summary" footerDescription="Everything from 1 video"/>
             </main>
         </>
     )
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,31 +4,24 @@ import { Button } from "@/components/ui/button";
 import { Brain, GraduationCap } from 'lucide-react';
 import Typewriter from "@/components/common/TypeWriter";
 import Link from "next/link";
+import HubCard from "@/components/common/HubCard";
 
 export default function Home() {
     return(
         <>
             <main className="flex flex-col items-center mt-32">
-                <div className="pb-32 max-sm:pb-24">
-                    <h1 className="font-extrabold text-4xl max-md:text-3xl max-sm:text-2xl">
+                <div className="pb-12 max-sm:pb-6">
+                    <h1 className="font-extrabold text-4xl max-md:text-3xl max-sm:text-2xl text-center">
                         Welcome to
                         <Typewriter text=" TechVideo.AI" infinite delay={150}/>
                     </h1>
+
+                    <p className="mt-6 text-center"><Typewriter text="A web app to assist you with your media using AI" delay={150}/></p>
                 </div>
 
-                <div className="flex justify-between h-96 w-1/2 gap-96 max-sm:gap-10 max-xl:gap-32 max-xl:w-4/6">
-                    <Link href={"/creator/hub"} passHref legacyBehavior>
-                        <Button className="w-1/3 h-1/3 pt-2 bg-purple-950 flex flex-col hover:bg-purple-600 max-xl:3/4 max-lg:w-1/2 max-sm:h-1/4">
-                            <Brain size={70} className="mb-6 mt-1 max-sm:mb-3"/>
-                            CREATOR
-                        </Button> 
-                    </Link>
-                    <Link href={"/student/hub"} passHref legacyBehavior>
-                        <Button className="w-1/3 h-1/3 bg-purple-950 hover:bg-purple-500 flex flex-col max-xl:3/4 max-lg:w-1/2 max-sm:h-1/4">
-                            <GraduationCap size={70} className="mb-6 mt-1 max-sm:mb-3"/>
-                            STUDENT
-                        </Button>
-                    </Link>
+                <div className="gap-20 grid grid-cols-2 w-1/3 max-xl:w-1/2 max-sm:w-full max-md:px-10 my-10 mx-16">
+                    <HubCard icon="brain" linkTo="/creator/hub" name="CREATOR" hasHoverContent={false} />
+                    <HubCard icon="graduation-cap" linkTo="/student/hub" name="STUDENT" hasHoverContent={false} />
                 </div>
             </main>
         </>
