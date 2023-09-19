c4e342aa99281688110ef856baf785c34dd9ad03
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-09-19 14:57:17 -0400

    adjust theme button

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -5,24 +5,22 @@ import { Button } from "@/components/ui/button"
 import { Separator } from '@/components/ui/separator'
 import Link from 'next/link'
 import {useTheme} from 'next-themes'
-import { Switch } from '@/components/ui/switch'
-import { Label } from '@/components/ui/label'
+import Icons from '../Icons'
 
 export default function Header(){
-    const {theme, setTheme} = useTheme()
+    const {theme, setTheme} = useTheme();
+
+    const toggleTheme = () => theme == 'dark' ? setTheme('light') : setTheme('dark');
     
     return (
         <>
             <header className="flex justify-between px-16 pt-8 pb-4 border-b-2 border-purple-950 max-sm:p-4">
                 <Link href={'/'} className="text-lg self-center">
-                    TechVideo.AI   
+                    TechVideo.AI
                 </Link>
                 <div className='flex items-center gap-4'>
-                    <span>
-                        <Switch id='theme-mode' onCheckedChange={() => theme == 'dark' ? setTheme('light'): setTheme('dark')}/>
-                        <Label htmlFor="theme-mode">{theme?.charAt(0).toUpperCase() + theme?.slice(1)} mode</Label>
-                        {/* <span onClick={() => setTheme('light')}>set light</span>
-                        <span onClick={() => setTheme('dark')}>set dark</span> */}
+                    <span onClick={toggleTheme}>
+                        <Icons iconName={theme === "light" ? "moon" : "sun"} size={20} overrideClass addClass='inline' />
                     </span>
                     <h6 className='text-muted-foreground text-sm max-sm:hidden'>Developed by ryan-albuquerque</h6>
                     <Separator orientation='vertical' className="h-6 max-sm:hidden"/>
