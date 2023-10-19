a0ccbbce41f442c6196a101f7b72eead2722bdc3
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-10-19 12:25:03 -0400

    add redux logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { logger } from "redux-logger";
 import videoReducer from "./video/reducer";
 
 const store = configureStore({
   reducer: {
     video: videoReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
