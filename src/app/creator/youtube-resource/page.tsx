803fee372c6612ad90383357094165911fd949a3
Ryan Albuquerque <56835754+Ryan-Albuquerque@users.noreply.github.com>
2023-10-23 20:37:00 -0400

    add zustand and task process to avoid gateway timeout

--- src/app/creator/youtube-resource/page.tsx
+++ src/app/creator/youtube-resource/page.tsx
@@ -1,38 +1,19 @@
-"use client"
-
-import { Separator } from "@/components/ui/separator"
-import { useState } from "react"
-import CreatorVideoPromptSection from "@/components/common/CreatorVideoPromptSection"
-import {StatusToButtonEnum} from '@/utils/enum/StatusToButtonEnum'
-import CreatorVideoUploadForm from "@/components/common/CreatorVideoUploadForm"
-import CreatorVideoOptionsForm from "@/components/common/CreatorVideoOptionsForm"
-
+import { Separator } from "@/components/ui/separator";
+import CreatorVideoPromptSection from "@/components/common/CreatorVideoPromptSection";
+import CreatorVideoUploadForm from "@/components/common/CreatorVideoUploadForm";
+import CreatorVideoOptionsForm from "@/components/common/CreatorVideoOptionsForm";
 
 export default function YoutubeResourceCreator() {
-    //status
-    const [uploadVideoStatus, setUploadVideoStatus] = useState<StatusToButtonEnum>(0)
-
-    //video upload
-    const [videoId, setVideoId] = useState<string>()
-    
-    //media generator
-    const [videoPrompt, setVideoPrompt] = useState<string>('video_description')
-
-    const [prompt, setPrompt] = useState<string>()
-
-    //completion/result
-    const [completion, setCompletion] = useState<string | undefined>()
-    
-    return(
-        <>
-            <main className="flex-1 flex min-h-screen max-sm:flex-col">
-                <aside className="w-1/3 max-lg:w-2/5 max-sm:w-full max-sm:px-2 flex flex-col space-y-8 p-5">
-                    <CreatorVideoUploadForm setId={setVideoId} setUploadStatus={setUploadVideoStatus} uploadStatus={uploadVideoStatus} />
-                    <Separator/>
-                    <CreatorVideoOptionsForm videoId={videoId} promptValue={prompt} uploadStatus={uploadVideoStatus} getCompletion={setCompletion} generatorType={videoPrompt} setGeneratorType={setVideoPrompt} />
-                </aside>
-                <CreatorVideoPromptSection completionValue={completion} setPromptValue={setPrompt} videoPromptOptions={videoPrompt}/>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <main className="flex-1 flex min-h-screen max-sm:flex-col">
+        <aside className="w-1/3 max-lg:w-2/5 max-sm:w-full max-sm:px-2 flex flex-col space-y-8 p-5">
+          <CreatorVideoUploadForm />
+          <Separator />
+          <CreatorVideoOptionsForm />
+        </aside>
+        <CreatorVideoPromptSection />
+      </main>
+    </>
+  );
 }
---
+++ src/lib/taskProcess.ts
@@ -0,0 +1,46 @@
+import { ApiStatus } from "@/utils/enum/ApiStatus";
+import { api } from "./api";
+const delay = 1000 * 15; //15s
+
+const watchTask = async (id: string) => {
+  let response, error;
+  let counter = 0;
+  while (counter <= 4) {
+    try {
+      const statusResponse = await api.get(`/task-status/${id}`);
+      const status = statusResponse.data.task.status;
+
+      if (status === ApiStatus.DONE || status === ApiStatus.ERROR) {
+        response =
+          status === ApiStatus.DONE ? statusResponse.data.task.data : null;
+        error =
+          status === ApiStatus.ERROR ? statusResponse.data.task.errors : null;
+        break;
+      }
+
+      // Delay
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      counter++;
+    } catch (e) {
+      error = e;
+    }
+  }
+
+  return { response, error };
+};
+
+export const startTask = async (urlPath: string, body: object) => {
+  const { data } = await api.post(urlPath, body);
+
+  const { response, error } = await watchTask(
+    data.taskId ?? data.result.taskId
+  );
+
+  if (error) {
+    new Error(
+      error?.response?.data?.error || error?.message || "Error in process task"
+    );
+  }
+
+  return response;
+};
